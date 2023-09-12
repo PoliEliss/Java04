44894f826717e7f96bd0391328cf5c5217945019
Polina <poli.eliss@mail.ru>
2023-09-12 22:45:37 +0300

    java lesson1

--- task-manager/src/ru/t1/eliseeva/tm/Application.java
+++ task-manager/src/ru/t1/eliseeva/tm/Application.java
@@ -1,3 +1,5 @@
+package ru.t1.eliseeva.tm;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Application {
