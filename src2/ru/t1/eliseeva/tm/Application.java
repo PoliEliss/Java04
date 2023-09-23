6dd41494b2871fa2ddfd08068a32e5a170ce2def
Polina <poli.eliss@mail.ru>
2023-09-23 12:27:46 +0300

    Доработка приложения Task Manager

--- src2/ru/t1/eliseeva/tm/Application.java
+++ src2/ru/t1/eliseeva/tm/Application.java
@@ -1,33 +1,82 @@
 package ru.t1.eliseeva.tm;
 
 import ru.t1.eliseeva.tm.constant.TerminalConst;
+import ru.t1.eliseeva.tm.constant.ArgumentConst;
+
+import java.util.Scanner;
 
 import static ru.t1.eliseeva.tm.constant.TerminalConst.*;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         parseArguments(args);
+        parseCommands();
     }
-    private static void parseArguments(final String[] args){
-        if (args == null || args.length == 0) showWelcome();
+
+    private static void parseCommands() {
+        showWelcome();
+        final Scanner scanner = new Scanner(System.in);
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.println("ENTER COMMAND");
+            final String command = scanner.next();
+            parseCommand(command);
+
+        }
+    }
+
+    private static void parseArguments(final String[] args) {
+        if (args == null || args.length == 0) return;
         final String arg = args[0];
         parseArgument(arg);
     }
+
+    private static void parseCommand(final String command) {
+        if (command == null || command.isEmpty()) return;
+        switch (command) {
+            case TerminalConst.VERSION:
+                showVersion();
+                break;
+            case TerminalConst.HELP:
+                showHelp();
+                break;
+            case TerminalConst.INFO:
+                showDeveloper();
+                break;
+            case TerminalConst.EXIT:
+                exit();
+                break;
+            default:
+                showCommandError();
+        }
+    }
+
+    private static void showCommandError() {
+        System.err.println("[ERROR]");
+        System.err.println("This command is not supported..");
+    }
+
     private static void parseArgument(final String arg) {
         if (arg == null || arg.isEmpty()) return;
         switch (arg) {
-            case VERSION:
+            case ArgumentConst.VERSION:
                 showVersion();
                 break;
-            case HELP:
+            case ArgumentConst.HELP:
                 showHelp();
                 break;
-            case INFO:
+            case ArgumentConst.INFO:
                 showDeveloper();
                 break;
             default:
-                showWelcome();
+                showArgumentError();
         }
+        System.exit(0);
+    }
+
+    private static void showArgumentError() {
+        System.err.println("[ERROR]");
+        System.err.println("This argument is not supported..");
+        System.exit(1);
     }
 
     private static void showDeveloper() {
@@ -36,8 +85,12 @@ public class Application {
     }
 
     private static void showWelcome() {
-        System.out.println("Привет!");
+        System.out.println("*** WELCOME TO TASK MANAGER ***");
+    }
+
+    private static void exit() {
         System.exit(0);
+
     }
 
     private static void showVersion() {
---
+++ src2/ru/t1/eliseeva/tm/constant/ArgumentConst.java
@@ -0,0 +1,9 @@
+package ru.t1.eliseeva.tm.constant;
+
+public class ArgumentConst {
+    public static final String INFO = "-i";
+    public static final String VERSION = "-v";
+
+    public static final String HELP = "-h";
+    public static final String EXIT = "-e";
+}
